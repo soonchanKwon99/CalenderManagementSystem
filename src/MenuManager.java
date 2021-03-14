6984ca3b4c3595443dc41eb30a2e62f87203b30a
Kwonsoonchan <kwonchan990317@gmail.com>
2021-03-15 01:33:15 +0900

    removed 'student' and replaced 'schedule'

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add Schedule");
+		System.out.println("2. Delete Schedule");
+		System.out.println("3. Edit Schedule");
+		System.out.println("4. View Schedule");
 		System.out.println("5. Exit");
 
 
