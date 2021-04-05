d7c4f5acd2758af98f0d7e8fd9e74e9c43cb9d19
Kwonsoonchan <kwonchan990317@gmail.com>
2021-04-05 21:44:30 +0900

    Made two more classes and connected each classes to interact.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,69 +1,35 @@
 import java.util.Scanner;
 
 public class MenuManager {
-
 	public static void main(String[] args) {
-		
-		int num = 0;
 		Scanner input = new Scanner(System.in);
+		CalendarManager calendarManager = new CalendarManager(input);		
 		
-		while (num != 6) {
+		int num = -1;
+		while (num != 5) {
 			System.out.println("***Calendar Management System***");
 			System.out.println("1. Add Schedule");
 			System.out.println("2. Delete Schedule");
 			System.out.println("3. Edit Schedule");
 			System.out.println("4. View Schedule");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6:");
+			System.out.println("5. Exit");
+			System.out.println("Select one number between 1-5:");
 			num = input.nextInt();
 			if (num == 1) {
-				addSchedule();
+				calendarManager.addSchedule();
 			}	
 			else if (num == 2) {
-				delSchedule();
+				calendarManager.delSchedule();
 			}
 			else if (num == 3) {
-				editSchedule();
+				calendarManager.editSchedule();
 			}
 			else if (num == 4) {
-				viewSchedule();
+				calendarManager.viewSchedule();
 			}
-			else if (num == 6) {
+			else {
 				System.out.println("Exit the Program");
 			}
 		}
-	}
-	public static void addSchedule() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Schedule Name:");
-		String ScheName = input.nextLine();
-		System.out.println(ScheName);
-		System.out.println("Schedule Date (DDMMMYY / ex: 20MAR21):");
-		String ScheDate = input.nextLine();
-		System.out.println(ScheDate);
-		System.out.println("Schedule Time (24hour-clock / ex: 1300):");
-		String ScheTime = input.nextLine();
-		System.out.println(ScheTime);
-		System.out.println("Schedule Location:");
-		String ScheLoca = input.nextLine();
-		System.out.println(ScheLoca);
-	}
-	public static void delSchedule() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Schedule Name:");
-		String ScheName = input.nextLine();
-	}
-	public static void editSchedule() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Schedule Name:");
-		String ScheName = input.nextLine();
-		System.out.println(ScheName);
-	}
-	public static void viewSchedule() {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Schedule Name:");
-		String ScheName = input.nextLine();
-		System.out.println(ScheName);
 	}
 }	
